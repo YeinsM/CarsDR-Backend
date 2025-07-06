b292722d4dbe065671ee2e4f9643fe35baa0b947
YeinsM <wildermancera1@gmail.com>
2025-07-05 21:42:08 -0400

    chore: Update Program.cs for improved service registration and middleware configuration

--- CarSpot.WebApi/Program.cs
+++ CarSpot.WebApi/Program.cs
@@ -52,7 +52,7 @@ var app = builder.Build();
 app.UseMiddleware<ExceptionMiddleware>();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsProduction())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
